e85673e5be8b5d50267846f701ddf7e4c10076a8
TatianaSH <tanyacholokova@gmail.com>
2015-02-12 21:42:43 -0500

    fix

--- js/app.js
+++ js/app.js
@@ -15,126 +15,44 @@ var SpeedEnemy = function(){
 SpeedEnemy.prototype = Object.create(Enemy.prototype);
 SpeedEnemy.prototype.constructor = SpeedEnemy;
 SpeedEnemy.prototype.render = function() {
-// 1) Create a canvas, either on the page or simply in code
-/*var canvas = document.createElement('canvas');
-//canvas.style.background ="transparent";
-var ctx2    = canvas.getContext('2d');
+
+var canvas = document.createElement('canvas');
+
+var ctx2  = canvas.getContext('2d');
 var image = new Image();
-image.crossOrigin = "anonymous";
-image.onload = start;
 //ctx2.globalAlpha   = 0.4;
 
-// 2) Copy  image data into the canvas
-function start(){
 ctx2.drawImage( Resources.get(this.sprite), 0, 0 );
-recolor(-.33);}
-// 3) Read image data
-function recolor(colorshift){
 var wigth = 101, higth= 171;
 var imageData = ctx2.getImageData(0,0,wigth,higth);
-var data = imageData.data;
+greyScale(imageData);
 
-// 4) Read or manipulate the rgba 
-for (var i=0;i<data.length;i+=4)
-  {
- var red = data[i+0];
- var green = data[i+1];
- var blue = data[i+2];
- var alpha = data[i+3];
+function greyScale(imageData) {
+    for (var i=0;i<imageData.data.length;i+=4)
+    {
+        var r = imageData.data[i+0];
+        var g = imageData.data[i+1];
+        var b = imageData.data[i+2];
+        var alpha = imageData.data[i+3];
 
- if (alpha < 200){
-    continue;
 
- }
- var hsl = rgbToHsl (red, green, blue);
- var hue = hsl.h * 360;
+        var grey = (r+g+b)/3;
+        //if(grey == 255) grey = 0;
+        imageData.data[i+0] = grey;
+        imageData.data[i+1] = grey;
+        imageData.data[i+2] = grey;
 
- if (hue > 200 && hue < 300){
-    var newRgb = hslToRgb(hsl.h + colorshift, hsl.s, hsl.l);
-    data[i+0] = newRgb.r;
-    data[i+1] = newRgb.g;
-    data[i+2] = newRgb.b;
-    data[i+3] = 255;
- }
-}
-
- /*var grey = (r+g+b)/3;
- //if(grey == 255) grey = 0;
- imageData.data[i+0] = grey;
- imageData.data[i+1] = grey;
- imageData.data[i+2] = grey;
-
- //imageData.data[i+3] = 0;
- //if(grey > 10) imageData.data[i+3] = 128;
-  //else imageData.data[i+3]=128;
-}*/
-
-// 5) Update the context with newly-modified data
-//ctx.putImageData(imageData,this.x, this.y);
-
-   ctx.drawImage(Resources.get(this.sprite), this.x, this.y); 
-}
-
-/*function rgbToHsl(r, g, b) {
-    r /= 255, g /= 255, b /= 255;
-    var max = Math.max(r, g, b),
-        min = Math.min(r, g, b);
-    var h, s, l = (max + min) / 2;
-
-    if (max == min) {
-        h = s = 0; // achromatic
-    } else {
-        var d = max - min;
-        s = l > 0.5 ? d / (2 - max - min) : d / (max + min);
-        switch (max) {
-            case r:
-                h = (g - b) / d + (g < b ? 6 : 0);
-                break;
-            case g:
-                h = (b - r) / d + 2;
-                break;
-            case b:
-                h = (r - g) / d + 4;
-                break;
-        }
-        h /= 6;
     }
-
-    return ({
-        h: h,
-        s: s,
-        l: l,
-    });
 }
 
+/*imageData.data[i+3] = 0;
+if(grey > 10) imageData.data[i+3] = 128;
+else imageData.data[i+3]=128;*/
+ctx.putImageData(imageData, this.x, this.y)
+}
+   
 
-function hslToRgb(h, s, l) {
-    var r, g, b;
 
-    if (s == 0) {
-        r = g = b = l; // achromatic
-    } else {
-        function hue2rgb(p, q, t) {
-            if (t < 0) t += 1;
-            if (t > 1) t -= 1;
-            if (t < 1 / 6) return p + (q - p) * 6 * t;
-            if (t < 1 / 2) return q;
-            if (t < 2 / 3) return p + (q - p) * (2 / 3 - t) * 6;
-            return p;
-        }
-
-        var q = l < 0.5 ? l * (1 + s) : l + s - l * s;
-        var p = 2 * l - q;
-        r = hue2rgb(p, q, h + 1 / 3);
-        g = hue2rgb(p, q, h);
-        b = hue2rgb(p, q, h - 1 / 3);
-    }
-
-    return ({
-        r: Math.round(r * 255),
-        g: Math.round(g * 255),
-        b: Math.round(b * 255),
-    });*/
     
     // Update the enemy's position, required method for game
 // Parameter: dt, a time delta between ticks
